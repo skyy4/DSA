a8325dbf0ffbd354c1531ca487ae6951df5a2aa3
skyy4 <124292376+skyy4@users.noreply.github.com>
2025-01-30 22:49:55 +0530

    day5

---
+++ reversePartArr.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class reversePartArr{
+    public static void main (String [] args){
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        int arr [] = new int [n];
+        System.out.print("Enter the first index : ");
+        int s = sc.nextInt(); 
+        System.out.print("Enter the other index : ");
+        int e = sc.nextInt();
+        for(int i = 0  ; i < n ; i++){       //input
+            arr[i]= sc.nextInt();
+        }
+        
+
+        if (s < 0 || e >= n || s > e) {
+            System.out.println("Invalid indices! Make sure 0 ≤ s ≤ e < " + n);
+        } else {
+            revArr(arr, s, e); 
+            System.out.println("Modified array:");    //output
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[j] + " ");
+            }
+        }
+
+        
+    }
+
+    public static void revArr(int arr[],int s,int e){
+      int sp = s;
+      int ep= e;
+      while(sp<ep){
+        int temp = arr[sp];
+        arr[sp]= arr[ep];
+        arr [ep]= temp;
+        sp++;
+        ep--;
+      }
+
+    }
+}
