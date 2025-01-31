0aef32a4a23daaf8a3912bc5e472e741287ae65e
skyy4 <124292376+skyy4@users.noreply.github.com>
2025-01-31 08:39:25 +0530

    day6

---
+++ SumOFfirstN.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class SumOFfirstN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = n*(n+1)/2 ;
+        System.err.println(sum);
+        
+    }
+    
+}
