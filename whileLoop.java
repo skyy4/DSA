26ca3bff28d3960b775b7a60807ba90d836e07eb
skyy4 <124292376+skyy4@users.noreply.github.com>
2025-02-02 23:15:31 +0530

    update

--- whileLoop.java
+++ whileLoop.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class whileLoop {
     public static void main(String [] args){
         int i = 1;
-        while(int i<=5){
+        while(i<=5){
             System.out.println(i);
             i++;
         }
